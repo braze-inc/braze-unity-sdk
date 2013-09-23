9bdfc59e8118a059971dee10b9dc7ae49846eab9
Wenzhi <wenzhi@appboy.com>
2013-09-23 15:34:44 -0400

    try to fix the wrong formatting due to tab

--- Plugins/Appboy/models/ApplePushNotification.cs
+++ Plugins/Appboy/models/ApplePushNotification.cs
@@ -8,17 +8,17 @@ namespace Appboy {
     // These properties are from the aps dictionary at the top level of the Apple Push Notification.
     // Note that these are actually nested inside the 'aps' key when sent from Apple, but we've
 	// included them in the top level ApplePushNotification upon deserialization for convenience.
-	public ApplePushNotificationAlert Alert { get; set; }
-	public int Badge { get; set; }
-	public string Sound { get; set; }
-	public int ContentAvailable { get; set; }
+    public ApplePushNotificationAlert Alert { get; set; }
+    public int Badge { get; set; }
+    public string Sound { get; set; }
+    public int ContentAvailable { get; set; }
 		
 	// All additional properties included with the notification alongside the 'aps' dictionary at
 	// the top level of the Apple Push Notification. See 
 	// https://developer.apple.com/library/ios/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/Chapters/ApplePushService.html#//apple_ref/doc/uid/TP40008194-CH100-SW10
-	public IDictionary<string, object> Extra { get; set; }
+    public IDictionary<string, object> Extra { get; set; }
 		
-	public ApplePushNotification(JSONClass json) {
+    public ApplePushNotification(JSONClass json) {
 	  Alert = new ApplePushNotificationAlert(json["alert"].AsObject);
 	  Badge = json["badge"].AsInt;
 	  Sound = json["sound"];
@@ -26,47 +26,45 @@ namespace Appboy {
 	  Extra = new Dictionary<string, object>();
 	  JSONNode ExtraNode = json["extra"];
 	  if (ExtraNode.GetType() == typeof(JSONClass)) {
-		foreach (KeyValuePair<string, JSONNode> KeyValuePairNode in ExtraNode.AsObject) {
+	    foreach (KeyValuePair<string, JSONNode> KeyValuePairNode in ExtraNode.AsObject) {
 		  Extra.Add(KeyValuePairNode.Key, this.getJSONNodeValue(KeyValuePairNode.Value));		
-		}		
+	    }		
 	  } else {
-		
-		// TODO: need to find a way to log error message
-		Debug.Log("Value of key Extra isn't a dictionary. Stop parsing the Extra dictionary");     
+	    Debug.Log("Value of key Extra isn't a dictionary. Stop parsing the Extra dictionary");     
 	  }
 	}
 		
     private object getJSONNodeValue(JSONNode Node) {
 	  int IntNode = 0;
 	  if (int.TryParse(Node.Value, out IntNode)) 
-		return IntNode;
+	    return IntNode;
 			
 	  float FloatNode = 0.0f;
 	  if (float.TryParse(Node.Value, out FloatNode))
-		return FloatNode;
+	    return FloatNode;
 			
 	  double DoubleNode = 0.0;
 	  if (double.TryParse(Node.Value, out DoubleNode))
-		return DoubleNode;
+	    return DoubleNode;
 			
 	  bool BoolNode = false;
 	  if (bool.TryParse(Node.Value, out BoolNode))
-		return BoolNode;
+	    return BoolNode;
 	  
 	  if (Node.GetType() == typeof(JSONArray)) {
-		IList<object> NodeList = new List<object>();
-		foreach (JSONNode NodeInArray in Node.AsArray) {
+	    IList<object> NodeList = new List<object>();
+	    foreach (JSONNode NodeInArray in Node.AsArray) {
 		  NodeList.Add(this.getJSONNodeValue(NodeInArray));
-		}
+	    }
 	    return NodeList;
 	  }
 	
 	  if (Node.GetType() == typeof(JSONClass)) {
-		IDictionary<string, object> NodeDictionary = new Dictionary<string, object>();
-		foreach (KeyValuePair<string, JSONNode> KeyValuePairNode in Node.AsObject) {
+	    IDictionary<string, object> NodeDictionary = new Dictionary<string, object>();
+	    foreach (KeyValuePair<string, JSONNode> KeyValuePairNode in Node.AsObject) {
 		  NodeDictionary.Add(KeyValuePairNode.Key, this.getJSONNodeValue(KeyValuePairNode.Value));		
-		}
-		return NodeDictionary;
+	    }
+	    return NodeDictionary;
 	  }
 			
 	  // if the Node isn't one type of int/float/double/bool/list/dictionary, then we should save it as a string
@@ -74,7 +72,7 @@ namespace Appboy {
 	}
 	
 			
-	public override string ToString() {
+    public override string ToString() {
 	  string extraString = "{";
       foreach (KeyValuePair<string, object> N in Extra) {
         if (extraString.Length > 2)
