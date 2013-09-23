83c0d52214e42fef8f9a057290f6128684182659
Wenzhi <wenzhi@appboy.com>
2013-09-23 15:19:38 -0400

    parse the whole extra dictionary from push notification

--- Plugins/Appboy/models/ApplePushNotification.cs
+++ Plugins/Appboy/models/ApplePushNotification.cs
@@ -1,3 +1,4 @@
+using UnityEngine;
 using System;
 using Appboy;
 using System.Collections.Generic;
@@ -23,9 +24,56 @@ namespace Appboy {
 	  Sound = json["sound"];
 	  ContentAvailable = json["content-available"].AsInt;
 	  Extra = new Dictionary<string, object>();
-	  Extra.Add("extra", json["extra"].ToString());
+	  JSONNode ExtraNode = json["extra"];
+	  if (ExtraNode.GetType() == typeof(JSONClass)) {
+		foreach (KeyValuePair<string, JSONNode> KeyValuePairNode in ExtraNode.AsObject) {
+		  Extra.Add(KeyValuePairNode.Key, this.getJSONNodeValue(KeyValuePairNode.Value));		
+		}		
+	  } else {
+		
+		// TODO: need to find a way to log error message
+		Debug.Log("Value of key Extra isn't a dictionary. Stop parsing the Extra dictionary");     
+	  }
 	}
 		
+    private object getJSONNodeValue(JSONNode Node) {
+	  int IntNode = 0;
+	  if (int.TryParse(Node.Value, out IntNode)) 
+		return IntNode;
+			
+	  float FloatNode = 0.0f;
+	  if (float.TryParse(Node.Value, out FloatNode))
+		return FloatNode;
+			
+	  double DoubleNode = 0.0;
+	  if (double.TryParse(Node.Value, out DoubleNode))
+		return DoubleNode;
+			
+	  bool BoolNode = false;
+	  if (bool.TryParse(Node.Value, out BoolNode))
+		return BoolNode;
+	  
+	  if (Node.GetType() == typeof(JSONArray)) {
+		IList<object> NodeList = new List<object>();
+		foreach (JSONNode NodeInArray in Node.AsArray) {
+		  NodeList.Add(this.getJSONNodeValue(NodeInArray));
+		}
+	    return NodeList;
+	  }
+	
+	  if (Node.GetType() == typeof(JSONClass)) {
+		IDictionary<string, object> NodeDictionary = new Dictionary<string, object>();
+		foreach (KeyValuePair<string, JSONNode> KeyValuePairNode in Node.AsObject) {
+		  NodeDictionary.Add(KeyValuePairNode.Key, this.getJSONNodeValue(KeyValuePairNode.Value));		
+		}
+		return NodeDictionary;
+	  }
+			
+	  // if the Node isn't one type of int/float/double/bool/list/dictionary, then we should save it as a string
+	  return Node.ToString();
+	}
+	
+			
 	public override string ToString() {
 	  string extraString = "{";
       foreach (KeyValuePair<string, object> N in Extra) {
