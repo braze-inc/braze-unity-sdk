5ba7bef87271012521d7b00fe902ee25be945bbc
Jared Poelman <poelman.jared@gmail.com>
2019-03-28 19:33:16 -0400

    Updating Unity plugins

--- unity-samples/Assets/Firebase/Editor/generate_xml_from_google_services_json.py
+++ unity-samples/Assets/Firebase/Editor/generate_xml_from_google_services_json.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python
 
-# Copyright 2016 Google Inc. All Rights Reserved.
+# Copyright 2016 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 """Stand-alone implementation of the Gradle Firebase plugin.
 
@@ -21,7 +33,7 @@ DEFAULT_INPUT_FILENAME = 'app/google-services.json'
 # Output filename if it isn't set.
 DEFAULT_OUTPUT_FILENAME = 'res/values/googleservices.xml'
 # Input filename for .plist files, if it isn't set.
-DEFAULT_PLIST_INPUT_FILENAME = 'GoogleServices-Info.plist'
+DEFAULT_PLIST_INPUT_FILENAME = 'GoogleService-Info.plist'
 # Output filename for .json files, if it isn't set.
 DEFAULT_JSON_OUTPUT_FILENAME = 'google-services-desktop.json'
 
